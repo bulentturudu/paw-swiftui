164b86a155efc9e09391872d7b9b371a09c260f4
Bülent TÜRÜDÜ <120729324+bulentturudu@users.noreply.github.com>
2025-07-13 10:56:09 +0300

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Bülent TÜRÜDÜ on 13.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
