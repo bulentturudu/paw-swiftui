25b70701d8fe72deb9b2f8f42c6bbb2fb9dca686
Bülent TÜRÜDÜ <120729324+bulentturudu@users.noreply.github.com>
2025-07-27 13:03:13 +0300

    düzenlemeler

---
+++ Paws/CustomContentUnavaibleView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomContentUnavaibleView.swift
+//  Paws
+//
+//  Created by Bülent TÜRÜDÜ on 15.07.2025.
+//
+
+import SwiftUI
+
+struct CustomContentUnavaibleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomContentUnavaibleView()
+}
---
+++ Paws/EditPetView.swift
@@ -0,0 +1,18 @@
+//
+//  EditPetView.swift
+//  Paws
+//
+//  Created by Bülent TÜRÜDÜ on 16.07.2025.
+//
+
+import SwiftUI
+
+struct EditPetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditPetView()
+}
